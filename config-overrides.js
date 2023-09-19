1cefc512099a9f59b609b939b67b73b8d94763be
Amu <amu230703@gmail.com>
2023-09-19 18:58:51 +0800

    [Feature] 修改環境參數

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,11 @@ const path = require('path')
 const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 
 module.exports = function override (config) {
+  console.log(config.output)
+  config.output.publicPath = process.env.NODE_ENV === 'production'
+    ? '/practice-react/'
+    : '/'
+
   config.resolve.alias = {
     ...config.resolve.alias,
     '@': path.resolve(__dirname, 'src'),
