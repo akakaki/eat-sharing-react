c42bb2f49e40972a96c8714804771f7fbe434fd9
Amu <amu230703@gmail.com>
2023-09-21 03:03:29 +0800

    [Feature] 更新 roter 與 icon-2

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,13 @@ import Routes from './route.js'
 
 // TODO: https://juejin.cn/post/7185897452862439485
 
+const basename = process.env.NODE_ENV === 'production'
+  ? '/practice-react/'
+  : '/'
+
 export default function routerProvider () {
   return (
-    <BrowserRouter basename={ process.env.PUBLIC_URL }>
+    <BrowserRouter basename={ basename }>
       <Routes />
     </BrowserRouter>
   )
