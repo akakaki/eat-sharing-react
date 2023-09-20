8d8fdc622a4c8821c22942768b5d4faf518fe90d
Amu <amu230703@gmail.com>
2023-09-20 18:58:05 +0800

    調整 router-2

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import route from './route.js'
 
 const router = createBrowserRouter(route, {
-  basename: process.env.NODE_ENV === 'production'
-    ? '/practice-react'
-    : '/'
+  // basename: process.env.NODE_ENV === 'production'
+  //   ? '/practice-react'
+  //   : '/'
+  basename: '/practice-react',
 })
 
 export default function routerProvider () {
